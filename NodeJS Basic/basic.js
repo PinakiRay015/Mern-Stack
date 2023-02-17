2835a320bf75cc545a493c049defa247236564de
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-17 10:14:27 +0530

    Node js

---
+++ NodeJS Basic/basic.js
@@ -0,0 +1,16 @@
+const greet = (name) => //creating function
+{
+    console.log(name);
+}
+
+//calling function
+// greet("Rohan") 
+
+//interval function
+// globalThis.setInterval(()=>
+// {
+//     console.log("Hello , Welcome to the mern stack course");
+// },2000)
+
+console.log(__dirname);
+console.log(__filename);
